548110a673088aec7d2c1218cf8f301774d26d62
Fisico <fisico994@gmail.com>
2021-09-16 22:13:22 +0200

    added lights

--- src/views/Lights.js
+++ src/views/Lights.js
@@ -84,6 +84,8 @@ export default function Lights({ HOST_IP, API_KEY }) {
 
   return (
     <div className="content">
+      <div className="inner">
+        <div className="devicecontainer">
       {type !== "none" && (
         <Flash
           type={type}
@@ -126,6 +128,8 @@ export default function Lights({ HOST_IP, API_KEY }) {
           />
         ))}
       </div>
+    </div>
+    </div>
     </div>
   );
 }
