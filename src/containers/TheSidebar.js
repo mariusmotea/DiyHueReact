11c7fa577d7901c19ad1809baa6b65c60b77140e
U-PAYPOINT\MoteaM <MariusMotea@paypoint.com>
2021-09-27 12:25:34 +0300

    set active on menu <li>

--- src/containers/TheSidebar.js
+++ src/containers/TheSidebar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { memo, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import {
   FaHome,
   FaLightbulb,
@@ -18,6 +19,8 @@ import logo from "../static/images/logo.svg";
 import "../scss/sidebar.scss";
 
 const TheSidebar = ({ showSidebar }) => {
+
+  const [currentElement, setCurrentElement] = useState(window.location.hash.substring(2));
   return (
     <div className={`columnLeft ${showSidebar ? "" : "active"}`}>
       <div className="topbarLeft">
@@ -27,63 +30,75 @@ const TheSidebar = ({ showSidebar }) => {
       <div className="sidebar">
       <ul>
         <a href="#home">
-          <li>
+          <li className={`${currentElement === "groups" ? "active" : ""}`}
+          onClick={() => setCurrentElement("groups")}>
             <FaHome style={{ color: "#0092FF" }} /> <p>Groups</p>
           </li>
         </a>
         <a href="#lights">
-          <li>
+          <li className={`${currentElement === "lights" ? "active" : ""}`}
+          onClick={() => setCurrentElement("lights")}>
             <FaLightbulb style={{ color: "#FF9E00" }} /> <p>Lights</p>
           </li>
         </a>
         <a href="#linkbutton">
-          <li>
+          <li className={`${currentElement === "linkbutton" ? "active" : ""}`}
+          onClick={() => setCurrentElement("linkbutton")}>
             <FaLink style={{ color: "#9b59b6" }} /> <p>Link Button</p>
           </li>
         </a>
         <a href="#bridge">
-          <li>
+          <li className={`${currentElement === "bridge" ? "active" : ""}`}
+          onClick={() => setCurrentElement("bridge")}>
             <Diybridge style={{ color: "#92FFFF" }} /> <p>Bridge</p>
           </li>
         </a>
         <a href="#devices">
-          <li>
+          <li className={`${currentElement === "devices" ? "active" : ""}`}
+          onClick={() => setCurrentElement("devices")}>
             <MdSettingsRemote style={{ color: "#7f8c8d" }} /> <p>Devices</p>
           </li>
         </a>
         <a href="#mqtt">
-          <li>
+          <li className={`${currentElement === "mqtt" ? "active" : ""}`}
+          onClick={() => setCurrentElement("mqtt")}>
             <Zigbee style={{ color: "#0084FF" }} /> <p>MQTT</p>
           </li>
         </a>
         <a href="#deconz">
-          <li>
+          <li className={`${currentElement === "deconz" ? "active" : ""}`}
+          onClick={() => setCurrentElement("deconz")}>
             <Deconz style={{ color: "#42A138" }} /> <p>Deconz</p>
           </li>
         </a>
         <a href="#tradfri">
-          <li>
+          <li className={`${currentElement === "tradfri" ? "active" : ""}`}
+          onClick={() => setCurrentElement("tradfri")}>
             <Tradfri style={{ color: "#CCA138" }} />{" "}
             <p>Tradfri</p>
           </li>
         </a>
         <a href="#hue">
-          <li>
+          <li className={`${currentElement === "hue" ? "active" : ""}`}
+          onClick={() => setCurrentElement("hue")}>
             <Bridge style={{ color: "#8400FF" }} /> <p>Hue Bridge</p>
           </li>
         </a>
         <a href="#alarm">
-          <li>
+          <li className={`${currentElement === "alarm" ? "active" : ""}`}
+          onClick={() => setCurrentElement("alarm")}>
             <FaExclamationTriangle style={{ color: "#c0392b" }} /> <p>Alarm</p>
           </li>
         </a>
         <a href="#settings">
-          <li>
+          <li className={`${currentElement === "settings" ? "active" : ""}`}
+          onClick={() => setCurrentElement("settings")}>
             <FaCog style={{ color: "#bdc3c7" }} /> <p>Settings</p>
           </li>
         </a>
         <a href="#about">
-          <li>
+          <li className={`${currentElement === "about" ? "active" : ""}`}
+          onClick={() => setCurrentElement("about")}>
             <FaInfoCircle style={{ color: "#fff" }} /> <p>About</p>
           </li>
         </a>
@@ -98,4 +113,4 @@ const TheSidebar = ({ showSidebar }) => {
   );
 };
 
-export default React.memo(TheSidebar);
+export default memo(TheSidebar);
