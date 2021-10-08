5256a35046b7ee6206039c22b2dcc7f19ec86fa1
Fisico <fisico994@gmail.com>
2021-10-08 21:45:44 +0200

    about page

--- src/views/About.js
+++ src/views/About.js
@@ -39,19 +39,14 @@ const About = () => {
         </div>
       </div>
 
-
-
       <div className="creditGrid">
-        
 
         <div className="contactCard">
           <div className="name">Marius</div>
           <div className="position">Main Developer & Mastermind of DiyHue</div>
-          <div className="about">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</div>
+          <div className="about">Developed and maintaining basically everything. Thousands of hours spent for reverse engineering, bug fixing and trying to implement every possible feature in his project.</div>
           <div className="iconbox">
             <a href="https://github.com/mariusmotea"><FaGithub /></a>
-            <a href="https://twitter.com"><FaTwitter /></a>
-            <a href="https://diyhue.org"><FaGlobeEurope /></a>
           </div>
         </div>
 
@@ -77,8 +72,8 @@ const About = () => {
 
         <div className="contactCard">
           <div className="name">David</div>
-          <div className="position">Designer - UI/UX</div>
-          <div className="about">Designed and frontend developed the user interface/experience. Design and producing music(<a href="https://spaceflightmemories.com">Spaceflight Memories Music</a>) is, what his life is all about.</div>
+          <div className="position">Designer</div>
+          <div className="about">Designed and frontend developed the user interface and user experience. Design and producing music (<a href="https://spaceflightmemories.com">Spaceflight Memories Music</a>) is, what his life is all about.</div>
           <div className="iconbox">
             <a href="https://github.com/fisico"><FaGithub /></a>
             <a href="https://twitter.com/sfmdavid"><FaTwitter /></a>
@@ -88,7 +83,7 @@ const About = () => {
 
         <div className="contactCard">
           <div className="name">Phil</div>
-          <div className="position">React Consultant</div>
+          <div className="position">React consultant</div>
           <div className="about">A very special thank you to Phil for consulting us with everything React related.</div>
           <div className="iconbox">
             <a href="https://github.com/philharmonie"><FaGithub /></a>
@@ -97,7 +92,7 @@ const About = () => {
 
         <div className="contactCard">
           <div className="name">Thank you!</div>
-          <div className="position">Contributors:</div>
+          <div className="position">A big thank you to everyone contributing to this project:</div>
           contributor, contributor, contributor, contributor, contributor, contributor, contributor, contributor, contributor, contributor, contributor, contributor,
         </div>
 
